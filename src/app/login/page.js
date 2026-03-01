5d1630101b2877a8ecd818631359d8a8bb168a62
Abhi solanki <abhi85076@gmail.com>
2026-03-01 15:45:30 +0530

    basic setup done

---
+++ src/app/login/page.js
@@ -0,0 +1,17 @@
+export default function LoginPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 ">
+            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
+                <h1 className="text-2xl font-bold text-center text-gray-800 mb-2">
+                    EduNest
+                </h1>
+                <p className="text-center text-gray-500 mb-6">
+                    School Management Portal
+                </p>
+                <p className="text-center text-gray-400">
+                    Login form coming soon ...
+                </p>
+            </div>
+        </div>
+    )
+}
