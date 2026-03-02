7e082b4e132fa67044db80aacaa7b2c745dd8246
Abhi solanki <abhi85076@gmail.com>
2026-03-02 21:41:14 +0530

    feat: Auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,2 +1,87 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from 'bcryptjs'
+import connectDB from "@/lib/mongodb";
+import User from "@/models/User";
+import Email from "next-auth/providers/email";
+
+export const authOptions = {
+    providers: [
+        Credentials({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                try {
+                    await connectDB()
+
+                    const user = await User.findOne({
+                        email: credentials.email,
+                        isActive: true
+                    })
+
+                    if (!user) {
+                        throw new Error("No user found with this email")
+                    }
+
+                    // Check password
+                    const isPasswordValid = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    )
+
+                    if (!isPasswordValid) {
+                        throw new Error("Invalid password")
+                    }
+
+                    // return user object
+                    return {
+                        id: user._id.toString(),
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                        avatar: user.avatar,
+                    }
+                } catch (error) {
+                    throw new Error(error.message)
+                }
+            }
+        })
+    ],
+
+    callbacks: {
+        async jwt({ token , user }) {
+            if (user) {
+                token.id = user.id;
+                token.role = user.role;
+                token.avatar = user.avatar;
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.role = token.role;
+                session.user.avatar = token.avatar;
+            }
+            return session
+        }
+    },
+
+    pages: {
+        signIn: "/login",
+        error: "/login",
+    },
+
+    session: {
+        strategy: 'jwt',
+    },
+
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+
+const handler = NextAuth(authOptions) ;
+export { handler as GET, handler as POST}
