724170ce97dd905c9ddc614bedca7bd72898398a
Abhi solanki <abhi85076@gmail.com>
2026-03-04 16:02:31 +0530

    Added: Card component from shadcn ui

--- src/app/dashboard/admin/page.js
+++ src/app/dashboard/admin/page.js
@@ -1,4 +1,18 @@
+'use client'
+import { useSession } from "next-auth/react"
+import { useState, useEffect } from "react"
+import { Users, GraduationCap, Heart } from "lucide-react"
+
 export default function AdminDashboard() {
+
+    const { data: session } = useSession()
+    const [stats, setStats] = useState({
+        totalTeachers : 0,
+        totalStudents : 0,
+        totalParents : 0,
+    }) 
+    const [loading, setLoading] = useState(true)
+
     return (
         <div>
             <p>Admin Dashboard Working</p>
