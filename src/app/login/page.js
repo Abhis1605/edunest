01b39f7206e40b64a4a17dbe733b3aa486cef3a1
Abhi solanki <abhi85076@gmail.com>
2026-04-05 17:25:57 +0530

    Added: showpassword button

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
+import { Eye, EyeOff } from "lucide-react"
 
 export default function LoginPage() {
     const router = useRouter()
@@ -10,6 +11,7 @@ export default function LoginPage() {
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
 
     const handleLogin = async (e) => {
         e.preventDefault()
@@ -80,11 +82,28 @@ export default function LoginPage() {
                 </div>
 
                 <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-2">
-                        Password :
-                    </label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#16737E] text-black" />
-                </div>
+    <label className="block text-sm font-medium text-gray-700 mb-2">
+        Password :
+    </label>
+
+    <div className="relative">
+        <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            className="w-full px-4 py-2 pr-10 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#16737E] text-black"
+        />
+
+        <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-2.5 text-gray-400 hover:text-gray-700"
+        >
+            { showPassword ? <EyeOff className="w-5 h-5 text-[#699640]" /> : <Eye className="h-5 w-5" />}
+        </button>
+    </div>
+</div>
 
                 <button onClick={handleLogin} disabled={loading} className="w-full bg-[#16737E] text-white py-2 rounded-lg font-medium hover:bg-[#2FA3B0] transition-colors disabled:opacity-50 disabled:cursor-not-allowed mt-2">
                     { loading ? 'Logging in...' : 'Login'}
